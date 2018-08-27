f7ef91521bbf7f673ebc056e8a9236ecc6ce1325
anan4 <39746052+anan4@users.noreply.github.com>
2018-08-27 21:46:29 +0200

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -1,5 +1,7 @@
 ## See data preparation code is in file "plot1.r"
 
+## NOTE: weekdays are in Catalan: dj. = thu, dv. = fri, ds. = sat
+
 # plot 2
 png("plot2.png")
 plot(dset$Time,dset$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
