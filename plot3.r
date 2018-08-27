17f930446c58f07163036ad114f57709716c6549
anan4 <39746052+anan4@users.noreply.github.com>
2018-08-27 21:31:37 +0200

    Create plot3.r

---
+++ plot3.r
@@ -0,0 +1,10 @@
+## See data preparation code is in file "plot1.r"
+
+# plot 3
+png("plot3.png")
+plot(dset$Time,dset$Sub_metering_1 , type = "n", xlab = "", ylab = "Energy sub metering")
+lines(dset$Time,dset$Sub_metering_1, col="black")
+lines(dset$Time,dset$Sub_metering_2, col="red")
+lines(dset$Time,dset$Sub_metering_3, col="blue")
+legend("topright",pch = intToUtf8(8212),col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
